15f90091f44b860bdc031ce4ef697ad3507427b5
Alexandre <alexsales001@hotmail.com>
2023-10-20 17:41:00 -0300

    deploy configs

--- config/puma.rb
+++ config/puma.rb
@@ -19,6 +19,8 @@ worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 #
 port ENV.fetch('PORT', 3000)
 
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
+
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch('RAILS_ENV', 'development')
