18e91393e7f019d3849cd373e350c075c99e8dc8
Alexandre <alexsales001@hotmail.com>
2023-10-21 04:16:56 -0300

    rubocop

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
     name { 'John Doe' }
-  email { Faker::Internet.unique.email }
-  password { 'securepassword' }
-  password_confirmation { 'securepassword' }
+    email { Faker::Internet.unique.email }
+    password { 'securepassword' }
+    password_confirmation { 'securepassword' }
 
     # If you have additional attributes, add them here.
     # For instance:
